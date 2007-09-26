f2b46dbafa07a4628a9007901cfa039d2ecc8451
comimom <comimom@gmail.com>
2007-09-26 03:04:30 +0000

    * Add sql_fixup to add primary key into id_submission_env   * Modify settings.py to add idsubmit to admin interface   * Delete the hardcoded local settings for staging area of I-Ds   * The staging location is obtained from database. One must update Staging path and Staging url via admin interface (or sql client) of I-D Submission Tool Env variables.  Staging path directory must be able to be accessed by Staging url   * Add maxlength attribute for a char field   * Modify views, forms, and templates html to apply staging path and staging url
    [[Split portion of a mixed commit.]]

--- sql_fixup.sql
+++ sql_fixup.sql
@@ -1,4 +1,5 @@
 -- This file holds needed corrections to the database until they have been applied to
 -- the live database.  This file is applied after importing a new dump of the live DB.
 
-
+-- To add primary key into id_submission_env table
+alter table id_submission_env add id int(1) primary key auto_increment first;

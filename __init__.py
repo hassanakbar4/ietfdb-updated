dc9de4d49bb2b76c7f1e4253bf06d6ac457e2189
Michael Lee <michael.lee@neustar.biz>
2008-01-10 00:52:27 +0000

    delete idsubmit branch
    [[Split portion of a mixed commit.]]

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright The IETF Trust 2007, All Rights Reserved
-
-__version__ = "2.xx-trunk"
-
-__date__    = "$Date$"
-
-__rev__     = "$Rev$ (dev) Latest release: Rev. 838 "
-
-__id__      = "$Id$"

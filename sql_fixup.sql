e181ce90c993773251ff1290992fc95ce1b30b1f
Michael Lee <michael.lee@neustar.biz>
2008-01-10 00:52:27 +0000

    delete idsubmit branch
    [[Split portion of a mixed commit.]]

--- sql_fixup.sql
+++
@@ -1,5 +0,0 @@
--- This file holds needed corrections to the database until they have been applied to
--- the live database.  This file is applied after importing a new dump of the live DB.
-
--- To add primary key into id_submission_env table
-alter table id_submission_env add id int(1) primary key auto_increment first;

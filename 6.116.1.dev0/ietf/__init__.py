09629b12807383a3c05388c0de7d8efb54f18eab
Peter E. Yee <github@houseofyee.com>
2020-01-22 23:26:39 +0000

    Fixing issues arising in other ballot types

---
+++ 6.116.1.dev0/ietf/__init__.py
@@ -0,0 +1,19 @@
+# Copyright The IETF Trust 2007-2020, All Rights Reserved
+# -*- coding: utf-8 -*-
+
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from . import checks                           # pyflakes:ignore
+
+# Don't add patch number here:
+__version__ = "6.116.1.dev0"
+
+# set this to ".p1", ".p2", etc. after patching
+__patch__   = ""
+
+__date__    = "$Date$"
+
+__rev__     = "$Rev$"
+
+__id__      = "$Id$"

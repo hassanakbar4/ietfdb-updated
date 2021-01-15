994969d2ad25542235c2ccd7f26602f0d4736741
Robert Sparks <rjsparks@nostrum.com>
2021-01-15 20:28:38 +0000

    Branch for maintaining the timezone-aware migration work.
    [[Split portion of a mixed commit.]]

--- ietf/utils/models.py
+++
@@ -1,9 +0,0 @@
-# Copyright The IETF Trust 2015, All Rights Reserved
-
-from django.db import models
-
-class DumpInfo(models.Model):
-    date = models.DateTimeField()
-    host = models.CharField(max_length=128)
-    tz   = models.CharField(max_length=32, default='UTC')
-    

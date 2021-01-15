e26a60687f63a4aee562932573c56f5b691fecde
Robert Sparks <rjsparks@nostrum.com>
2021-01-15 20:28:38 +0000

    Branch for maintaining the timezone-aware migration work.
    [[Split portion of a mixed commit.]]

--- ietf/dbtemplate/urls.py
+++
@@ -1,9 +0,0 @@
-
-
-from ietf.dbtemplate import views
-from ietf.utils.urls import url
-
-urlpatterns = [
-    url(r'^(?P<acronym>[-a-z0-9]+)/$', views.template_list),
-    url(r'^(?P<acronym>[-a-z0-9]+)/(?P<template_id>[\d]+)/$', views.template_edit),
-]
--- ietf/group/templatetags/group_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def has_sessions(group,num):
-    return group.session_set.filter(meeting__number=num).exists()
-

003a12f4058fbdb7e262ff04cf5f97525054c2ce
Robert Sparks <rjsparks@nostrum.com>
2021-01-15 20:28:38 +0000

    Branch for maintaining the timezone-aware migration work.
    [[Split portion of a mixed commit.]]

--- ietf/stats/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-from ietf.stats import views
-from ietf.utils.urls  import url
-
-urlpatterns = [
-    url("^$", views.stats_index),
-    url("^review/(?:(?P<stats_type>completion|results|states|time)/)?(?:%(acronym)s/)?$" % settings.URL_REGEXPS, views.review_stats),
-]
